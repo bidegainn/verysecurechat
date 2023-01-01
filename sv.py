6830a9ee828bd0ead3b510b03c5a9babd861d68c
juancruzbidegain@gmail.com <juancruzbidegain@gmail.com>
2023-01-01 19:08:30 -0300

    change code sv and c.s

--- sv.py
+++ sv.py
@@ -1,1 +1,16 @@
-#sv - 
+#sv - #SERVER
+
+from socket import *
+
+address = "LOCALHOST"
+port = 9099
+
+#Parametros conexion TCP - sin perdida
+sck_server = socket(AF_INET, SOCK_STREAM)
+
+sck_server.bind(address,port)
+
+
+while True:
+	pass
+	
